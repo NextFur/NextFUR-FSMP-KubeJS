e181e2ebf332ee67d3d615a89e6ed2c0e1bb8db1
João Pedro (Diju) <jpdiju@gmail.com>
2025-11-20 20:06:24 -0300

    Update 2.0 Recipe Remover
    Removed recipes from
    AA
    Ex Deorum
    Industrial Foregoing

---
+++ server_scripts/remover/2_0remover.js
@@ -0,0 +1,27 @@
+ServerEvents.recipes(e => {
+    e.remove({ output:[
+        //Actually Additions
+        "actuallyadditions:ring_of_growth",
+        "actuallyadditions:ring_of_magnetizing",
+        "actuallyadditions:teleport_staff",
+        "actuallyadditions:phantom_connector",
+        //IndustrialForegoing
+        "industrialforegoing:infinity_backpack",
+        "industrialforegoing:infinity_trident",
+        "industrialforegoing:infinity_launcher",
+        "industrialforegoing:infinity_saw",
+        "industrialforegoing:infinity_hammer",
+        "industrialforegoing:infinity_drill",
+        "industrialforegoing:infinity_nuke",
+        "industrialforegoing:mechanical_dirt",
+        "industrialforegoing:ore_laser_base",
+         //ExDeorum AKA Ex Nihilo
+         "exdeorum:end_cake",
+         "exdeorum:string_mesh",
+         "exdeorum:flint_mesh",
+         "exdeorum:iron_mesh",
+         "exdeorum:golden_mesh",
+         "exdeorum:diamond_mesh",
+         "exdeorum:netherite_mesh"
+    ]})
+})
